a13b3546586d10c9ed7445141e489489826ecea9
roc-mini <cjdesyue@126.com>
2022-04-02 10:01:22 +0800

    手机型号

--- Sources/RCBase/Base/rColor.swift
+++ Sources/RCBase/Base/rColor.swift
@@ -9,15 +9,17 @@ import Foundation
 import UIKit
 
 struct rColor {
-    /// RGBA的颜色设置
+    /// RGB的颜色设置
     static func RGB(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
         return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: 1.0)
     }
 
+    /// RGBA的颜色设置
     static func RGBA(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) -> UIColor {
         return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
     }
 
+    /// 16位 颜色设置
     static func HexA(hexValue: Int, a: CGFloat) -> (UIColor) {
         return UIColor(red: (CGFloat)((hexValue & 0xFF0000) >> 16) / 255.0,
                        green: (CGFloat)((hexValue & 0xFF00) >> 8) / 255.0,
@@ -25,8 +27,8 @@ struct rColor {
                        alpha: a)
     }
 
+    /// 16位 颜色设置
     static func Hex(hexValue: Int) -> (UIColor) {
         return HexA(hexValue: hexValue, a: 1.0)
     }
 }
-
