e81a1746c689ca0671e9f024864c4ee884a6822c
roc-mini <cjdesyue@126.com>
2022-04-07 15:52:28 +0800

    feat

---
+++ RSwiftBase/Carthage/Build/RSwiftBase.xcframework/ios-arm64/RSwiftBase.framework/Headers/RSwiftBase.h
@@ -0,0 +1,18 @@
+//
+//  RSwiftBase.h
+//  RSwiftBase
+//
+//  Created by 林小鹏 on 2022/4/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RSwiftBase.
+FOUNDATION_EXPORT double RSwiftBaseVersionNumber;
+
+//! Project version string for RSwiftBase.
+FOUNDATION_EXPORT const unsigned char RSwiftBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSwiftBase/PublicHeader.h>
+
+
---
+++ RSwiftBase/RSwiftBase/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  RSwiftBase
+//
+//  Created by 林小鹏 on 2022/4/7.
+//
+
+import Foundation
+
+public class test: NSObject {
+    func test() {
+        debugPrint("测试")
+    }
+}
