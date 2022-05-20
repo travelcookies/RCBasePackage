23c5292e6ad092277c1ac72ea25b8be4ec054432
roc-mini <cjdesyue@126.com>
2022-05-20 15:00:04 +0800

    feat

--- RSwiftBase/Extension/UIView+NibLoadable.swift
+++ RSwiftBase/Extension/UIView+NibLoadable.swift
@@ -11,7 +11,7 @@ public protocol NibLoadable {
 
 public extension NibLoadable where Self: UIView {
     // 在协议里面不允许定义class 只能定义static
-    static func loadFromNib(_ nibname: String? = nil) -> Self { // Self (大写) 当前类对象
+    func loadFromNib(_ nibname: String? = nil) -> Self { // Self (大写) 当前类对象
         // self(小写) 当前对象
         let loadName = nibname == nil ? "\(self)" : nibname!
 
